3e4319ce6054fa81d3ed881e581141f4ae0b1ae3
Hernán Alperin <hernan.alperin@gmail.com>
2019-03-25 10:50:34 -0300

    Update como procesar un shp.sql

--- como procesar un shp.sql
+++ como procesar un shp.sql
@@ -1,23 +1,45 @@
+/*
 Hacer a mano:
 
-- createdb comuna11
-- vi dump.sql para hacer un drop table antes del begin para evitar error
-- psql comuna11
-- create extension postgis
-- psql -f dump.sql comuna11
+@prompt:
 
-- psql comuna11
+createdb comuna11
+vi dump.sql para hacer un drop table antes del begin para evitar error
+psql comuna11
+create extension postgis
+psql -f dump.sql comuna11
+ psql comuna11
+*/
 
-select ST_NumGeometries(geom), count(*)
+-- chequear consistencias, casos
+
+comuna11=#  select ST_NumGeometries(geom), count(*)
 from lineas
 group by ST_NumGeometries(geom)
 ;
-
-/* chequear consistencia
+/*
  st_numgeometries | count
 ------------------+-------
                 1 |  2840
 */
+
+-- qué tipo
+comuna11=# select distinct ST_GeomeTrytype(geom) from lineas;
+/*
+ st_geometrytype
+--------------------
+ ST_MultiLineString
+(1 fila)
+*/
+
+-- pasar a linestring para calcular extremos
+alter table lineas add column geom_ejes geometry;
+update lineas
+set geom_ejes = ST_LineMerge(geom)
+;
+
+
+
 
 
 
