0e1f5381b9ab3a512f90ac4c18efba6f83e649b5
Hernán Alperin <hernan.alperin@gmail.com>
2019-04-04 19:32:18 -0300

    Update segmentar listado.py

--- segmentar listado.py
+++ segmentar listado.py
@@ -4,27 +4,37 @@
 # este programa segmenta un circuito (manzana, o secuencia de lados) con el recorrido ordenado
 # sacado de una tabla sql
 
+# hecho para ejemplo comuna11,
+# TODO: generalizar
+
 import sys
 import psycopg2
 
-SQLConnect = sys.argv[1]
-TablaListado = sys.argv[2]
+TablaListado = sys.argv[1]
 # cotas de semgmentos
 Minimo, Maximo = 17, 23
 if len(sys.argv) > 4:
     Minimo, Maximo = int(sys.argv[3]), int(sys.argv[4])
 
-import urlparse # for python 3+ use: from urllib.parse import urlparse
-result = urlparse.urlparse("postgresql://postgres:postgres@localhost/postgres")
-username = result.username
-password = result.password
-database = result.path[1:]
-hostname = result.hostname
- = psycopg2.connect(
-    database = database,
-    user = username,
+SQLConnect = psycopg2.connect(
+    database = comuna11,
+    user = segmentador,
     password = password,
-    host = hostname
+    host = localhost
 )
+
+"""
+algunos select para ir probando...
+
+select depto, frac, radio, mnza, count(*)
+from listado
+-- factible TODO: revisar para caso deferente de Minimo, Maximo = 17, 23
+group by depto, frac, radio, mnza
+having count(*) >= 17
+and count(*) not between 24 and 33
+order by depto, frac, radio, mnza
+;
+
+"""
 
 
