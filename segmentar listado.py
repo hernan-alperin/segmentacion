03fe7e7be54fb0ed8a214c187fed2ed6d785ab0c
Hernán Alperin <hernan.alperin@gmail.com>
2019-04-08 13:43:07 -0300

    segmentando de la forma más fácil
    dividiendo a 20 el ranking

--- segmentar listado.py
+++ segmentar listado.py
@@ -66,4 +66,58 @@ where (comunas, frac_comun, radio_comu, mza_comuna) in (select * from posibles)
 
 """
 
+"""
+--- usnado windows para ayudar a calcular cortes
+create or replace view segmentando_facil as 
+with separados as (
+    SELECT frac_comun, radio_comu::integer, mza_comuna::integer, clado, hn, hp, hd,
+        row_number() OVER w as row, rank() OVER w as rank
+    FROM listado
+    WINDOW w AS (PARTITION BY comunas, frac_comun, radio_comu::integer, mza_comuna::integer
+    ORDER BY comunas, frac_comun, radio_comu::integer, mza_comuna::integer, clado,
+            case 
+                when hn::integer % 2 = 1 then hn::integer
+                else -hn::integer   
+            end, 
+    hp)
+    ),
+    sumados as (
+    select frac_comun, radio_comu::integer, mza_comuna::integer, count(*) as cant
+    from listado
+    group by comunas, frac_comun, radio_comu::integer, mza_comuna::integer
+    ),
+    parejo as (
+    select ceil(cant/20)*20 as redondo
+    from sumados
+    )
+select frac_comun, radio_comu, mza_comuna, clado, hn, hp, ceil(rank/20) + 1 as segmento_manzana
+from separados
+left join sumados
+using(frac_comun, radio_comu, mza_comuna)
+order by frac_comun, radio_comu::integer, mza_comuna::integer, clado,
+            case 
+                when hn::integer % 2 = 1 then hn::integer
+                else -hn::integer   
+            end, 
+    hp
+;
 
+alter table listado add column segmento_en_manzana integer;
+update listado l
+set segmento_en_manzana = segmento_manzana
+from segmentando_facil f
+where (l.frac_comun, l.radio_comu::integer, l.mza_comuna::integer, l.clado, l.hn, case when l.hp is Null then '' else l.hp end) = 
+    (f.frac_comun, f.radio_comu, f.mza_comuna, f.clado, f.hn, case when f.hp is Null then '' else f.hp end)
+;
+
+select frac_comun, radio_comu, mza_comuna, clado, hn, hp, segmento_en_manzana 
+from listado
+order by frac_comun, radio_comu, mza_comuna, clado,
+            case 
+                when hn::integer % 2 = 1 then hn::integer
+                else -hn::integer   
+            end, 
+            hp
+;
+
+"""
