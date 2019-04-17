aae53e1b9e80e615deadb4556630eb718b887140
Hernán Alperin <hernan.alperin@gmail.com>
2019-04-17 18:50:05 -0300

    hay que cortar en pisos...

--- selects.sql
+++ selects.sql
@@ -44,31 +44,12 @@ from comuna11
 order by frac_comun, radio_comu, mza_comuna, clado, id
 ;
 
-------------------------------------------------------
-drop view segmentando_equilibrado;
-create or replace view segmentando_equilibrado as 
-with deseado as (
-    select 40::float as deseado),
-    buscando_diferencias as (
-    select comunas, frac_comun, radio_comu::integer, mza_comuna::integer,
-           count(*) as vivs,
-           ceil(count(*)/deseado) as ceil_seg_mza,
-           floor(count(*)/deseado) as floor_seg_mza
-    from comuna11, deseado
-    group by comunas, frac_comun, radio_comu::integer, mza_comuna::integer, deseado
-    ),
-    deseado_manzana as (
-    select *, 
-        case 
-            when floor_seg_mza = 0 then vivs
-            else vivs < least(vivs/ceil_seg_mza - deseado, vivs/floor_seg_mza - deseado)
-        end as mas_cerca
-    from buscando_diferencias, deseado
-    )
-select *
-from deseado_manzana
-;
 
+-------------------------------------------------------------------
+-- calcula la cantidad de viviendas por segmento que menor se aparte
+-- de la catidad deseaado
+-------------------------------------------------------------------
+-- caso para testear
 
 with deseado as (
     select 40::float as deseado
@@ -77,19 +58,42 @@ with deseado as (
     select generate_series(1, 1000) as vivs
     ),
     posibles_segs_mza as (
-    select vivs, floor(vivs/deseado) as min, ceil(vivs/deseado) as max
+    select vivs, greatest(1, floor(vivs/deseado)) as min, ceil(vivs/deseado) as max
     from casos, deseado
+    ),
+    mejor_diferencia as (
+    select vivs, min, max, 
+           case when abs(vivs/max - deseado) < abs(vivs/min - deseado) then max
+           else min end as seg_x_mza
+    from posibles_segs_mza, deseado
     )
-select * from posibles_segs_mza
+select * from mejor_diferencia
 ;
-
-
-
-
-                
-                
+-----------------------------------------------------------------------------------
+-- separando listado por segmentos en manzanas independientes
+-- donde la distribución de viviendas en cada segmento en la manzana es equilibrado
+-- y rank es el orden de visita en el segmento
+-----------------------------------------------------------------------------------
+drop view segmentando_equilibrado;
+create or replace view segmentando_equilibrado as 
+with deseado as (
+    select 40::float as deseado),
+    casos as (
+    select comunas, frac_comun, radio_comu::integer, mza_comuna::integer,
+           count(*) as vivs,
+           ceil(count(*)/deseado) as max,
+           greatest(1, floor(count(*)/deseado)) as min
+    from comuna11, deseado
+    group by comunas, frac_comun, radio_comu::integer, mza_comuna::integer, deseado
+    ),
+    deseado_manzana as (
+    select comunas, frac_comun, radio_comu::integer, mza_comuna::integer, vivs, 
+        case when abs(vivs/max - deseado) < abs(vivs/min - deseado) then max
+        else min end as seg_x_mza
+    from casos, deseado
+    ),
     separados as (
-    SELECT frac_comun, radio_comu::integer, mza_comuna::integer, clado, hn, hp, hd, id, 
+    SELECT frac_comun, radio_comu::integer, mza_comuna::integer, clado, hn, hp, hd, id
         row_number() OVER w as row, rank() OVER w as rank
     FROM comuna11
     WINDOW w AS (PARTITION BY comunas, frac_comun, radio_comu::integer, mza_comuna::integer
@@ -99,20 +103,20 @@ select * from posibles_segs_mza
     select frac_comun, radio_comu::integer, mza_comuna::integer, count(*) as cant
     from comuna11
     group by comunas, frac_comun, radio_comu::integer, mza_comuna::integer
-    ),
-    parejo as (
-    select round(cant/deseado)*deseado as redondo
-    from sumados, deseado
     )
-select frac_comun, radio_comu, mza_comuna, clado, hn, hp, ceil(rank/maximo_manzana) as segmento_manzana
+select frac_comun, radio_comu, mza_comuna, clado, hn, hp, hd,
+    floor((rank - 1)*seg_x_mza/vivs) + 1 as segmento, rank
 from deseado_manzana
 join separados
 using(frac_comun, radio_comu, mza_comuna)
 left join sumados
 using(frac_comun, radio_comu, mza_comuna)
-order by frac_comun, radio_comu::integer, mza_comuna::integer, clado, id
+order by frac_comun, radio_comu::integer, mza_comuna::integer, clado,
+    floor((rank - 1)*seg_x_mza/vivs) + 1, rank
 ;
-
+                    
+--------------------------------------------------------
+                    
 select frac_comun, radio_comu, mza_comuna, segmento_manzana, count(*)
 from segmentando_equilibrado
 group by frac_comun, radio_comu, mza_comuna, segmento_manzana
