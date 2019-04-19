5da48267325f26b77e82c045df89f51698ea01f5
Hernán Alperin <hernan.alperin@gmail.com>
2019-04-19 00:32:50 -0300

    hasta abriendo los pisos y rankeando

--- cortar_a_deseado_por_manzana.sql
+++ cortar_a_deseado_por_manzana.sql
@@ -10,22 +10,34 @@ fecha: 2019-04-18 Ju
 drop view segmentando_facil;
 create or replace view segmentando_facil as 
 with deseado as (
-    select 40 as deseado),
-    separados as (
-    SELECT frac_comun, radio_comu::integer, mza_comuna::integer, clado, hn, hp, hd, id, 
-        row_number() OVER w as row, rank() OVER w as rank
-    FROM comuna11
-    WINDOW w AS (PARTITION BY comunas, frac_comun, radio_comu::integer, mza_comuna::integer
-    ORDER BY comunas, frac_comun, radio_comu::integer, mza_comuna::integer, clado, id)
+        select 40 as deseado
     ),
+    pisos_enteros as (
+        select frac_comun, radio_comu::integer, mza_comuna::integer, clado, min(id) as min_id, hn, hp
+        from comuna11
+        group by frac_comun, radio_comu::integer, mza_comuna::integer, clado, hn, hp
+    ),    
+    abriendo_pisos as (
+        select frac_comun, radio_comu::integer, mza_comuna::integer, clado, hn, hp, hd,
+            row_number() over w as row, rank() over w as rank
+        from pisos_enteros
+        natural join comuna11
+        window w as (
+            partition by frac_comun, radio_comu::integer, mza_comuna::integer
+            -- separa las manzanas
+            ORDER BY frac_comun, radio_comu::integer, mza_comuna::integer, clado, min_id, hp
+            -- rankea por piso (ordena hn como corresponde pares descendiendo)
+        )
+    )
+
+select * from abriendo_pisos;
+---- testeando esto debería separa por pisos
+    
+    ,
     sumados as (
     select frac_comun, radio_comu::integer, mza_comuna::integer, count(*) as cant
     from comuna11
     group by comunas, frac_comun, radio_comu::integer, mza_comuna::integer
-    ),
-    parejo as (
-    select ceil(cant/deseado)*deseado as redondo
-    from sumados, deseado
     )
 select frac_comun, radio_comu, mza_comuna, clado, hn, hp, ceil(rank/deseado) + 1 as segmento_manzana
 from deseado, separados
