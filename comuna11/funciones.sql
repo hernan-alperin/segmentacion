b32e7012872ebcd8717d076008a22f101754d58d
-h <alpe@alum.mit.edu>
2019-04-29 09:24:36 -0300

    fn opc 4 lado

--- comuna11/funciones.sql
+++ comuna11/funciones.sql
@@ -70,4 +70,40 @@ $$
 language sql
 ;
 
+drop function opciones_para_lado;
+create or replace function opciones_para_lado(
+    mza text,
+    lado integer)
+-- obtiene subllistado con el circuito de 2 manzanas
+-- resultado de cruzar a la manzana 2 luego de recorrer la manzana 1 
+-- en manzanas de 4 ejemplo dddvddd si esto es posible <= existe v
+-- TODO: ver si es exhaustivo de todos los circuitos posibles de 2 mzas
+returns table (
+    lado_id integer,
+    mza text,
+    lado integer,
+    ady_id integer,
+    mza_ady text,
+    lado_ady integer,
+    tipo_ady text
+    )
+as $$
+select lado_id, l.mza, l.lado::integer, 
+    g.lado_ady as ady_id, a.mza as mza_ady, a.lado::integer as lado_ady, tipo_ady 
+from lados_de_manzana l
+join grafo_adyacencias_lados g
+on l.id::integer = g.lado_id
+and l.mza = $1
+and l.lado = $2
+join lados_de_manzana a
+on a.id = g.lado_ady
+$$
+language sql
+;
+
+/*
+select * from opciones_para_lado('020770100101001',1);
+
+
+*/
 
