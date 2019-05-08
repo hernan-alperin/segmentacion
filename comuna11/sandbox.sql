1aab5071f460658631eb6e0325fd458aa1085b71
Hernán Alperin <hernan.alperin@gmail.com>
2019-05-08 14:49:32 -0300

    manzanas completas

--- comuna11/sandbox.sql
+++ comuna11/sandbox.sql
@@ -6,3 +6,17 @@ fecha: 2019-04-26 Vi
 */
 
 -- parte para funcioes sql y pgsql
+
+with segs as (
+  select frac_comun, radio_comu, mza_comuna, segmento_en_manzana_equilibrado as sgm, count(*) as vivs
+  from comuna11
+  group by frac_comun, radio_comu, mza_comuna, segmento_en_manzana_equilibrado)
+select frac_comun, radio_comu, mza_comuna, array_agg(sgm), vivs
+from segs
+group by frac_comun, radio_comu, mza_comuna, vivs
+having cardinality(array_agg(sgm)) = 1
+order by frac_comun, radio_comu, mza_comuna
+;
+
+
+
