0ecb7eb4103e33d0c2fe17df5d6781917d333e7e
Hernán Alperin <hernan.alperin@gmail.com>
2019-05-09 16:36:17 -0300

    estandarizacion frac radio

--- sgmentador/radios.sql
+++ sgmentador/radios.sql
@@ -1,6 +1,6 @@
 -- extrae todos los radios de la muestra de la comuna11
 
-select distinct frac_comun::integer, radio_comu::integer
+select distinct frac_comun::integer as frac, radio_comu::integer as radio
 from comuna11
 ;
 
