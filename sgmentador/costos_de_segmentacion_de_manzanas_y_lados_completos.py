a053ee419acee325b0d58c77da8c01d24f9c0e8e
Hernán Alperin <hernan.alperin@gmail.com>
2019-05-09 16:25:54 -0300

    Update costos_de_segmentacion_de_manzanas_y_lados_completos.py

--- sgmentador/costos_de_segmentacion_de_manzanas_y_lados_completos.py
+++ sgmentador/costos_de_segmentacion_de_manzanas_y_lados_completos.py
@@ -11,41 +11,15 @@ fecha: 2019-05-09 Ju
 # y relativas a la calidad del segmento y la segmentación
 #
 # caso 1
-cantidad_de_viviendas_deseada_por_segmento = 20
-cantidad_de_viviendas_maxima_deseada_por_segmento = 23
-cantidad_de_viviendas_minima_deseada_por_segmento = 17
-if len(sys.argv) > 5:
-    cantidad_de_viviendas_minima_deseada_por_segmento = int(sys.argv[4])
-    cantidad_de_viviendas_maxima_deseada_por_segmento = int(sys.argv[5])
-if len(sys.argv) > 6:
-    cantidad_de_viviendas_deseada_por_segmento = int(sys.argv[6])
+cantidad_de_viviendas_deseada_por_segmento = 40
+if len(sys.argv) > 4:
+    cantidad_de_viviendas_deseada_por_segmento = int(sys.argv[4])
 
 
 def costo(segmento):
     # segmento es una lista de manzanas
     carga_segmento = carga(segmento)
-    if carga_segmento > cantidad_de_viviendas_maxima_deseada_por_segmento:
-        # la carga es mayor el costo es el cubo
-        return ((carga_segmento - cantidad_de_viviendas_maxima_deseada_por_segmento)
-                *(carga_segmento - cantidad_de_viviendas_maxima_deseada_por_segmento)
-                *(carga_segmento - cantidad_de_viviendas_maxima_deseada_por_segmento)
-            + (carga_segmento - cantidad_de_viviendas_deseada_por_segmento))
-    elif carga_segmento < cantidad_de_viviendas_minima_deseada_por_segmento:
-        # la carga es menor el costo es el cuadrado
-        return ((cantidad_de_viviendas_deseada_por_segmento - carga_segmento)
-                *(cantidad_de_viviendas_deseada_por_segmento - carga_segmento)
-            + (carga_segmento - cantidad_de_viviendas_deseada_por_segmento))
-    else:  # está entre los valores deseados
-        # el costo el la diferencia absoluta al valor esperado
-        return abs(carga_segmento - cantidad_de_viviendas_deseada_por_segmento)
-    """
-    # otro caso, costo en rango, cuadrático por arriba y lineal por abajo
-    if carga_segmento > cantidad_de_viviendas_deseada_por_segmento:
-        return (carga_segmento - cantidad_de_viviendas_deseada_por_segmento)**4
-    else:
-        return (cantidad_de_viviendas_deseada_por_segmento - carga_segmento)**2
-    """
-
+    return abs(carga_segmento - cantidad_de_viviendas_deseada_por_segmento)
 
 #####################################################################################
 
