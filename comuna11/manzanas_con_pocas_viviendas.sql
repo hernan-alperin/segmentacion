db2cfbd65c34741ca7027070098e9a186b91264e
-h <alpe@alum.mit.edu>
2019-05-20 19:24:24 -0300

    comenta resultado

--- comuna11/manzanas_con_pocas_viviendas.sql
+++ comuna11/manzanas_con_pocas_viviendas.sql
@@ -31,6 +31,7 @@ select * from manzanas_con_pocas_viviendas(1,2,30);
  
 */
 
+/*
 with radios as (
     select distinct frac, radio 
     from comuna11)
@@ -38,7 +39,23 @@ select manzanas_con_pocas_viviendas(frac, radio, 30)
 from radios
 limit 10
 ;
+
 -- devuelve tuplas
 -- ver como haces que devuelva las columnas
+ manzanas_con_pocas_viviendas 
+------------------------------
+ (1,1,11,15)
+ (4,8,78,15)
+ (5,4,63,29)
+ (5,4,66,4)
+ (1,11,80,12)
+ (1,11,86,1)
+ (3,3,5,18)
+ (3,3,6,25)
+ (4,1,19,28)
+ (10,8,20,3)
+(10 rows)
+
+*/
 
 
