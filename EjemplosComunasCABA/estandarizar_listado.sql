934015e8fb4942c6f0629d48eea1194e9a639487
-h <alpe@alum.mit.edu>
2019-06-03 20:40:48 -0300

    ::integer

--- EjemplosComunasCABA/estandarizar_listado.sql
+++ EjemplosComunasCABA/estandarizar_listado.sql
@@ -13,9 +13,9 @@ fecha: 2019-06-03 Lu
 drop table if exists listados.caba;
 create table listados.caba as
 select id_0, id, 
-    "COMUNAS" depto, "IdBarrio" idbarrio, 
-    "FRAC_COMUN" frac, "RADIO_COMU" radio, 
-    "MZA_COMUNA" mza , "CLADO" lado, "CCODIGO" cod_calle,
+    "COMUNAS"::integer depto, "IdBarrio" idbarrio, 
+    "FRAC_COMUN"::integer frac, "RADIO_COMU"::integer radio, 
+    "MZA_COMUNA"::integer mza , "CLADO"::integer lado, "CCODIGO" cod_calle,
     "CNOMBRE" nombre_calle, "HN" numero, "H4" as h4, Null cuerpo, 
     "HP" piso, "HD" apt, "Nomencla-20" nomencla_20
 from listados.recorte_comunas_8_11_14_15
