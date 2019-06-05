4a1f1d9074bbd32a264ed958b54b9e0d5c4f6aec
-h <alpe@alum.mit.edu>
2019-06-05 09:14:08 -0300

    con ejemplo

--- EjemplosComunasCABA/arrays_recursivas.sql
+++ EjemplosComunasCABA/arrays_recursivas.sql
@@ -19,6 +19,7 @@ inner join mzas j
 on not (i.set_of_mza && j.set_of_mza)
 ;
 
+create table arrays_recursivas as
 with recursive conjuntos as (
     select set_of_mza from mzas
     union
@@ -30,4 +31,25 @@ with recursive conjuntos as (
 select * from conjuntos
 ;
 
+/*
+select * from arrays_recursivas;
+ set_of_mza 
+------------
+ {1}
+ {2}
+ {3}
+ {2,1}
+ {3,1}
+ {1,2}
+ {3,2}
+ {1,3}
+ {2,3}
+ {3,2,1}
+ {2,3,1}
+ {3,1,2}
+ {1,3,2}
+ {2,1,3}
+ {1,2,3}
+(15 rows)
+*/
 
