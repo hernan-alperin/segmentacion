78f665c17eb637f9a3b3faae2a8da0c0b9c6dcaa
-h <hernan.alperin@gmail.com>
2019-06-05 12:22:00 -0300

    como funcion

--- EjemplosComunasCABA/arrays_recursivas.sql
+++ EjemplosComunasCABA/arrays_recursivas.sql
@@ -53,3 +53,41 @@ select * from arrays_recursivas;
 (15 rows)
 */
 
+create or replace function sets_de_mzas(tbl_name text)
+returns table (set_of_mza integer[]) as $$
+begin
+return query execute
+format('    
+    with recursive conjuntos as (
+        select set_of_mza from %1$s
+        union
+        select i.set_of_mza || j.set_of_mza
+        from %1$s i
+        inner join conjuntos j
+        on not (i.set_of_mza && j.set_of_mza)
+    )
+    select * from conjuntos', $1);
+end
+$$ language plpgsql;
+
+/*
+select sets_de_mzas('mzas');
+ sets_de_mzas 
+--------------
+ {1}
+ {2}
+ {3}
+ {2,1}
+ {3,1}
+ {1,2}
+ {3,2}
+ {1,3}
+ {2,3}
+ {3,2,1}
+ {2,3,1}
+ {3,1,2}
+ {1,3,2}
+ {2,1,3}
+ {1,2,3}
+(15 rows)
+*/
