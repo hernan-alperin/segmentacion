6da5c495258edb58b4076a4b9c938c0d8ed67f37
-h <hernan.alperin@gmail.com>
2019-06-10 12:58:56 +0100

    sql con comentario

--- EjemplosComunasCABA/estandarizar.sql
+++ EjemplosComunasCABA/estandarizar.sql
@@ -20,7 +20,10 @@ select id_0, id,
     "MZA_COMUNA"::integer mza , "CLADO"::integer lado, "CCODIGO" cod_calle,
     "CNOMBRE" nombre_calle, "HN" numero, "H4" as h4, Null cuerpo, 
     "HP" piso, "HD" apt, "Nomencla-20" nomencla_20
-from recorte_comunas_8_11_14_15
+from 
+---------- Nombre de la tabla con los datos importados -----------
+recorte_comunas_8_11_14_15
+------------------------------------------------------------------
 where id is not Null
 ;
 
