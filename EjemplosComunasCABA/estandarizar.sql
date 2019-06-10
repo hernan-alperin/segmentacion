c3446372c898e68d9655ade9ed7e280bddee16da
-h <hernan.alperin@gmail.com>
2019-06-10 12:40:59 +0100

    no lo corrige, hay que mover archivo a /tmp

--- EjemplosComunasCABA/estandarizar.sql
+++ EjemplosComunasCABA/estandarizar.sql
@@ -8,6 +8,8 @@ autor:-h
 fecha: 2019-06-03 Lu
 */
 
+create schema if not exists listados;
+
 -- normalizar columnas de formato CABA
 -- frac_comun | radio_comu | mza_comuna --> frac | radio | mza
 drop table if exists listados.caba;
@@ -18,7 +20,7 @@ select id_0, id,
     "MZA_COMUNA"::integer mza , "CLADO"::integer lado, "CCODIGO" cod_calle,
     "CNOMBRE" nombre_calle, "HN" numero, "H4" as h4, Null cuerpo, 
     "HP" piso, "HD" apt, "Nomencla-20" nomencla_20
-from listados.recorte_comunas_8_11_14_15
+from recorte_comunas_8_11_14_15
 where id is not Null
 ;
 
