7b3aba484cbc88450fe9f43025bcefe1c4954859
-h <hernan.alperin@gmail.com>
2019-06-17 00:28:10 +0200

    renombra fn en particiones

--- sandbox/particiones.sql
+++ sandbox/particiones.sql
@@ -1,6 +1,6 @@
 -- agregar sets_adyacentes
 create temp sequence if not exists ids; 
 select nextval('ids') as id, *
-from blqs_de_mzas('mzas', 'adyacencias');
+from sets_de_adys('mzas', 'adyacencias');
 
 drop sequence ids;
