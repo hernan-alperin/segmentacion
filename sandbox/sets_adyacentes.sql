7e5f02e104184002fd882d5e795ed545b8b3e7f1
-h <hernan.alperin@gmail.com>
2019-06-16 19:28:12 +0200

    extension intarray para sort tiene un bug...

--- sandbox/sets_adyacentes.sql
+++ sandbox/sets_adyacentes.sql
@@ -1,61 +1,62 @@
-CREATE EXTENSION intarray;
+CREATE OR REPLACE FUNCTION array_sort (ANYARRAY)
+RETURNS ANYARRAY LANGUAGE SQL
+AS $$
+SELECT ARRAY(SELECT unnest($1) ORDER BY 1)
+$$;
+--https://stackoverflow.com/questions/2913368/sorting-array-elements
 
-drop table if exists mzas;
-create table mzas(
-    id integer
-);
-
-truncate mzas;
-insert into mzas values(1);
-insert into mzas values(2);
-insert into mzas values(3);
-insert into mzas values(4);
-
-drop table if exists adyacencias;
-create table adyacencias (
-    mza_i integer,
-    mza_j integer
-);
-
-truncate adyacencias;
-insert into adyacencias values (1,2);
-
-create or replace function blqs_de_mzas(mzas text, adyacencias text)
-returns table (set_de_mza integer[]) as $$
+create or replace function sets_de_adys(
+    compnts text, 
+    adyacencias text)
+returns table (set_compnt integer[]) as $$
 begin
 return query execute
-format('    
+format('
     with recursive conjuntos as (
         with sets as (
-            select Null as set_de_mza
+            select Null as set_compnt
             union
-            select array[id] from mzas
+            select array[id] from %1$s
             )
-        select set_de_mza from sets
+        select set_compnt from sets
         union
-        select i.set_de_mza || j.set_de_mza
+        select i.set_compnt || j.set_compnt
         from sets i
         inner join conjuntos j
-        on not (i.set_de_mza && j.set_de_mza)
-        --and i.set_de_mza[1] < all(j.set_de_mza) 
+        on not (i.set_compnt && j.set_compnt)
+        --and i.set_compnt[1] < all(j.set_compnt) 
     )
-    select distinct sort(set_de_mza) as blq from conjuntos
-    where array_length(set_de_mza,1) = 1
-    or array_length(set_de_mza,1) = 2
-        and (set_de_mza[1], set_de_mza[2]) in (select * from %2$s)
-    or array_length(set_de_mza,1) = 3
-        and (set_de_mza[1], set_de_mza[2]) in (select * from %2$s)
-        and (set_de_mza[2], set_de_mza[3]) in (select * from %2$s)
-    or array_length(set_de_mza,1) = 3
-        and (set_de_mza[1], set_de_mza[2]) in (select * from %2$s)
-        and (set_de_mza[1], set_de_mza[3]) in (select * from %2$s)
+    select distinct array_sort(set_compnt) as blq from conjuntos
+    where array_length(set_compnt,1) = 1
+    or array_length(set_compnt,1) >= 2
+        and (set_compnt[1], set_compnt[2]) in (select * from %2$s)
+    or array_length(set_compnt,1) >= 3
+        and ((set_compnt[1], set_compnt[3]) in (select * from %2$s)
+          or (set_compnt[2], set_compnt[3]) in (select * from %2$s)
+            )
+    or array_length(set_compnt,1) >= 4
+        and ((set_compnt[1], set_compnt[4]) in (select * from %2$s)
+          or (set_compnt[2], set_compnt[4]) in (select * from %2$s)
+          or (set_compnt[3], set_compnt[4]) in (select * from %2$s)
+            )
+    or array_length(set_compnt,1) >= 5
+        and ((set_compnt[1], set_compnt[5]) in (select * from %2$s)
+          or (set_compnt[2], set_compnt[5]) in (select * from %2$s)
+          or (set_compnt[3], set_compnt[5]) in (select * from %2$s)
+          or (set_compnt[4], set_compnt[5]) in (select * from %2$s)
+            )
+    or array_length(set_compnt,1) >= 6
+        and ((set_compnt[1], set_compnt[6]) in (select * from %2$s)
+          or (set_compnt[2], set_compnt[6]) in (select * from %2$s)
+          or (set_compnt[3], set_compnt[6]) in (select * from %2$s)
+          or (set_compnt[4], set_compnt[6]) in (select * from %2$s)
+          or (set_compnt[5], set_compnt[6]) in (select * from %2$s)
+            )
+    
     order by blq
     ', $1, $2);
 end
 $$ language plpgsql;
 --TODO: hacer una function recursiva sobre adyacencias
--- o poner un límite en la cantidad de mzas, lo que me parece mejor
+-- o poner un límite en la cantidad de compnts, lo que me parece mejor
 
-insert into adyacencias values(1,3);
-insert into adyacencias values(1,4);
-select blqs_de_mzas('mzas', 'adyacencias');
