f0815e13a8432a2d9ea4ad9e2ecb9ad0b0823792
Hernán Alperin <hernan.alperin@gmail.com>
2019-08-13 13:32:52 -0300

    ahora sí CREATE FUNCTION

--- PilotoHumahuaca/yLas4Chiquitas/cargar.sql
+++ PilotoHumahuaca/yLas4Chiquitas/cargar.sql
@@ -24,26 +24,26 @@ WITH listado_sin_vacios AS (
     -------------------- listado --------------------------
     listados.listado_humauaca_chiquitas
     -------------------------------------------------------
-    WHERE prov!='' AND dpto!=''  AND codloc!='' and frac!='' and radio!='' and mza !='' and lado!=''
+    WHERE prov!='''' AND dpto!=''''  AND codloc!='''' and frac!='''' and radio!='''' and mza !='''' and lado!=''''
 )
 , e00 as (
     SELECT codigo10, nomencla, codigo20, ancho, anchomed, tipo, nombre, ladoi, ladod, desdei, desded, hastai, hastad, mzai, mzad,
     codloc20, nomencla10, nomenclai, nomenclad, wkb_geometry,
     -------------------- nombre de covertura y tabla de shape
-    '$1'::text cover FROM '' || $1 || ''.arc
+    ''$1''::text cover FROM ' || $1 || '.arc
     ---------------------------------------------------------
 )
 , lados_de_manzana as (
-    select codigo20, mzai||'-'||ladoi as lado_id, mzai as mza, ladoi as lado, avg(anchomed) as anchomed,
+    select codigo20, mzai||''-''||ladoi as lado_id, mzai as mza, ladoi as lado, avg(anchomed) as anchomed,
         st_linemerge(st_union(st_reverse(wkb_geometry))) as geom, cover
     from e00
-    where mzai is not Null and mzai != ''
+    where mzai is not Null and mzai != ''''
     group by codigo20, mzai, ladoi, cover
     union
-    select codigo20, mzad||'-'||ladod as lado_id, mzad as mza, ladod as lado, avg(anchomed) as anchomed,
+    select codigo20, mzad||''-''||ladod as lado_id, mzad as mza, ladod as lado, avg(anchomed) as anchomed,
         st_linemerge(st_union(wkb_geometry)) as geom, cover
     from e00
-    where mzad is not Null and mzad != ''
+    where mzad is not Null and mzad != ''''
     group by codigo20, mzad, ladod, cover
 ),
 lados_codigos as (
@@ -58,7 +58,7 @@ lado_manzana AS (
         substring(mza,13,3)::integer as mza, 
         codigo20, lado_id, mza link, lado::integer, 
         geom, st_azimuth(st_startpoint(geom), st_endpoint(geom)) azimuth, cover,
-        CASE WHEN st_geometrytype(geom) != 'ST_LineString' THEN 'Lado discontinuo' END as error_msg
+        CASE WHEN st_geometrytype(geom) != ''ST_LineString'' THEN ''Lado discontinuo'' END as error_msg
     from lados_codigos
     ORDER BY mza, lado
 ), listado_carto AS (
@@ -67,16 +67,16 @@ lado_manzana AS (
     LEFT JOIN listado_sin_vacios USING (prov,dpto,codloc,frac,radio,mza,lado)
 )
 -- Conteo x lado de manzna
-SELECT row_number() OVER () gid,'$1.arc'::text shape, prov, dpto depto, codloc,
+SELECT row_number() OVER () gid,''$1.arc''::text shape, prov, dpto depto, codloc,
     frac, radio, mza, lado,
     count(CASE 
-          WHEN trim(cod_tipo_vivredef) in ('', 'CO', 'N', 'CA/', 'LO')
+          WHEN trim(cod_tipo_vivredef) in ('''', ''CO'', ''N'', ''CA/'', ''LO'')
             THEN NULL 
             ELSE cod_tipo_vivredef END) conteo
 insert INTO segmentacion.conteos
 FROM listado_carto
 GROUP BY prov, dpto, codloc, frac, radio, mza, lado, geom
-ORDER BY count(CASE WHEN trim(cod_tipo_vivredef)='' THEN NULL ELSE cod_tipo_vivredef END) desc'
+ORDER BY count(CASE WHEN trim(cod_tipo_vivredef)='''' THEN NULL ELSE cod_tipo_vivredef END) desc'
 ;
 end;
 $$
