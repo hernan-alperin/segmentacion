5e298895112251b5f60bd02e089568af84093844
Hernán Alperin <hernan.alperin@gmail.com>
2019-08-14 12:49:13 -0300

    Update generar_adyacencias.sql

--- PilotoHumahuaca/yLas4Chiquitas/generar_adyacencias.sql
+++ PilotoHumahuaca/yLas4Chiquitas/generar_adyacencias.sql
@@ -25,7 +25,8 @@ or tabla = 'e5759.arc'
 or tabla = 'e5760.arc'
 ;
 
---create table lados_de_manzana as
+--drop table arto.lados_de_manzana cascade;
+--create table carto.lados_de_manzana as
 with pedacitos_de_lado as (-- mza como PPDDDLLLFFRRMMMselect mzad as mza, ladod as lado, avg(anchomed) as anchomed,
     select mzad as mza, ladod as lado,
         array_agg(distinct tipo) as tipos,
@@ -57,9 +58,11 @@ with pedacitos_de_lado as (-- mza como PPDDDLLLFFRRMMMselect mzad as mza, ladod 
     group by mza, lado, tipos, codigos, calles
     order by mza, lado
     )
-insert into lados_de_manzana 
+insert into carto.lados_de_manzana 
 select row_number() over() as id, *,
     ST_StartPoint(lado_geom) as nodo_i_geom, ST_EndPoint(lado_geom) as nodo_j_geom
 from lados_orientados
 order by mza, lado
 ;
+
+                     
