9916e201419595abc6a66c740594f695d9bfaff9
Hernán Alperin <hernan.alperin@gmail.com>
2019-08-14 12:18:29 -0300

    Create generar_adyacenciar.sql

---
+++ PilotoHumahuaca/yLas4Chiquitas/generar_adyacenciar.sql
@@ -0,0 +1,7 @@
+drop table if exists lados_de_manzana;
+-- tabla con los ejes unidos, lados duplicado y dirigidos por
+-- mzad, ladod en sentido y mzai, ladoi en sentido contrario
+-- para respetar regla hombro derecho
+--
+
+create 
