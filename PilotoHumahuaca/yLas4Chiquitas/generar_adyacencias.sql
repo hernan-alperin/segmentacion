875c80674287aa24d5b4dade4a3b4c171233a84e
Hernán Alperin <hernan.alperin@gmail.com>
2019-08-14 13:01:53 -0300

    lados_de_manzana

--- PilotoHumahuaca/yLas4Chiquitas/generar_adyacencias.sql
+++ PilotoHumahuaca/yLas4Chiquitas/generar_adyacencias.sql
@@ -7,7 +7,9 @@ drop table if exists lados_de_manzana;
 create schema if not exists carto;
 grant usage on schema carto to segmentador;
 
+drop view if exists carto.arcos;
 create view carto.arcos as
+select row_number() over () fid , * from (
 select 'e3019.arc' as tabla, mzai, mzad, ladoi, ladod, tipo, codigo20, nombre, wkb_geometry from e3019.arc
 union
 select 'e5757.arc' as tabla, mzai, mzad, ladoi, ladod, tipo, codigo20, nombre, wkb_geometry from e5757.arc
@@ -15,7 +17,7 @@ union
 select 'e5759.arc' as tabla, mzai, mzad, ladoi, ladod, tipo, codigo20, nombre, wkb_geometry from e5759.arc
 union
 select 'e5760.arc' as tabla, mzai, mzad, ladoi, ladod, tipo, codigo20, nombre, wkb_geometry from e5760.arc
-;
+) as identificados;
 grant select on carto.arcos to segmentador;
 
 delete from carto.lados_de_manzana
@@ -25,19 +27,20 @@ or tabla = 'e5759.arc'
 or tabla = 'e5760.arc'
 ;
 
---drop table arto.lados_de_manzana cascade;
+--drop table carto.lados_de_manzana cascade;
 --create table carto.lados_de_manzana as
-with pedacitos_de_lado as (-- mza como PPDDDLLLFFRRMMMselect mzad as mza, ladod as lado, avg(anchomed) as anchomed,
-    select mzad as mza, ladod as lado,
+--
+with pedacitos_de_lado as (-- mza como PPDDDLLLFFRRMMM select mzad as mza, ladod as lado, avg(anchomed) as anchomed,
+    select tabla, mzad as mza, ladod as lado,
         array_agg(distinct tipo) as tipos,
         array_agg(distinct codigo20) as codigos,
         array_agg(distinct nombre) as calles,
         ST_Union(wkb_geometry) as geom_pedacito -- ST_Union por ser MultiLineString
     from carto.arcos
     where mzad is not Null and mzad != '' and ladod != 0
-    group by mzad, ladod
+    group by tabla, mzad, ladod
     union -- duplica los pedazos de lados a derecha e izquierda
-    select mzai as mza, ladoi as lado,
+    select tabla, mzai as mza, ladoi as lado,
         array_agg(distinct tipo) as tipos,
         array_agg(distinct codigo20) as codigos,
         array_agg(distinct nombre) as calles,
@@ -45,20 +48,22 @@ with pedacitos_de_lado as (-- mza como PPDDDLLLFFRRMMMselect mzad as mza, ladod 
         -- para respetar sentido hombro derecho
     from carto.arcos
     where mzai is not Null and mzai != '' and ladoi != 0
-    group by mzai, ladoi, tipo, codigo20, nombre
-    order by mza, lado--, tipo, codigo20, calle
+    group by tabla, mzai, ladoi
+    order by mza, lado
     ),
     lados_orientados as (
-    select mza as nomencla, 
+    select tabla, mza as nomencla, 
         substr(mza,9,2)::integer as frac, substr(mza,11,2)::integer as radio, 
         substr(mza,13,3)::integer as mza, lado,
         tipos, codigos, calles,
         ST_LineMerge(ST_Union(geom_pedacito)) as lado_geom -- une por mza,lado
     from pedacitos_de_lado
-    group by mza, lado, tipos, codigos, calles
-    order by mza, lado
+    group by tabla, mza, lado, tipos, codigos, calles
+    order by tabla, mza, lado
     )
+--
 insert into carto.lados_de_manzana 
+--
 select row_number() over() as id, *,
     ST_StartPoint(lado_geom) as nodo_i_geom, ST_EndPoint(lado_geom) as nodo_j_geom
 from lados_orientados
@@ -66,3 +71,4 @@ order by mza, lado
 ;
 
                      
+-
