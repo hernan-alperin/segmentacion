652c69e6517af554f4b7b2fd77ed2cedb29a69f9
Hernán Alperin <hernan.alperin@gmail.com>
2019-08-14 12:28:16 -0300

    Update generar_adyacenciar.sql

--- PilotoHumahuaca/yLas4Chiquitas/generar_adyacenciar.sql
+++ PilotoHumahuaca/yLas4Chiquitas/generar_adyacenciar.sql
@@ -4,4 +4,15 @@ drop table if exists lados_de_manzana;
 -- para respetar regla hombro derecho
 --
 
-create 
+create view carto.arcos as
+select * from e3019.arc
+union
+select * from e5757.arc
+union
+select * from e5759.arc
+union
+select * from e5760.arc
+;
+
+
+
