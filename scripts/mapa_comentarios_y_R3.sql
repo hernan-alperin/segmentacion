b09604e76f13a6998be65de77edfc75a46743120
Hernán Alperin <hernan.alperin@gmail.com>
2019-11-13 13:15:54 -0300

    una linea por segnmento

--- scripts/mapa_comentarios_y_R3.sql
+++ scripts/mapa_comentarios_y_R3.sql
@@ -2,7 +2,9 @@
 with 
   lados_por_mza as (
   select prov, depto, frac, radio, mza, count(*) as cant_lados
+-----------------------------------------------------------------  
   from e0359.listado_segmentos
+-----------------------------------------------------------------
   group by prov, depto, frac, radio, mza
   ),
   mzas_en_segmentos as (
@@ -16,27 +18,24 @@ with
   natural join 
   lados_por_mza
   where cant_lados = cant_lados_en_seg
-  )
-select prov, depto, frac, radio, seg, mza, 'manzana completa' as lado
-from mzas_completas
-union
-select prov, depto, frac, radio, seg, mza, 
-  replace(
-    replace(
-      replace(
-        array_agg(lado)::text, 
-        '{', 'lados '
-         ),
-      '}',''
-    ),
-    ',', ', '
-  )
-from e0359.listado_segmentos
-where (prov, depto, frac, radio, seg, mza) not in (
-  select prov, depto, frac, radio, seg, mza 
+  ),
+  descripcion_mza as (
+  select prov, depto, frac, radio, seg, 'manzana '||mza||' completa' as descripcion
   from mzas_completas
-)
-group by prov, depto, frac, radio, seg, mza
-order by prov, depto, frac, radio, seg, mza, lado
+  union
+  select prov, depto, frac, radio, seg, 'manzana '||mza||' '||
+    replace(replace(replace(array_agg(lado)::text, '{', 'lados '), '}',''), ',', ', ') as descripcion
+  from e0359.listado_segmentos
+  where (prov, depto, frac, radio, seg, mza) not in (
+    select prov, depto, frac, radio, seg, mza
+    from mzas_completas
+    )
+  group by prov, depto, frac, radio, seg, mza
+  order by prov, depto, frac, radio, seg, descripcion
+  )
+select prov, depto, frac, radio, seg, string_agg(descripcion,', ')
+from descripcion_mza
+group by prov, depto, frac, radio, seg
+order by prov, depto, frac, radio, seg
 ;
 
