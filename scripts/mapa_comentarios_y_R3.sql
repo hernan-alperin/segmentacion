040cb63cdaa49131c77f89d80311c6e8c4d9cbb2
Hernán Alperin <hernan.alperin@gmail.com>
2019-11-13 11:52:11 -0300

    segmento separado en mzas lados

--- scripts/mapa_comentarios_y_R3.sql
+++ scripts/mapa_comentarios_y_R3.sql
@@ -1,3 +1,4 @@
+
 with 
   lados_por_mza as (
   select prov, depto, frac, radio, mza, count(*) as cant_lados
@@ -16,12 +17,14 @@ with
   lados_por_mza
   where cant_lados = cant_lados_en_seg
   )
-select prov, depto, frac, radio, seg, mza, Null as lado
+select prov, depto, frac, radio, seg, mza, 'manzana completa' as lado
 from mzas_completas
 union
 select prov, depto, frac, radio, seg, mza, lado
 from e0359.listado_segmentos
-where (prov, depto, frac, radio, seg, mza) not in (select * from mzas_completas)
+where (prov, depto, frac, radio, seg, mza) not in (
+  select prov, depto, frac, radio, seg, mza from mzas_completas
+)
 order by prov, depto, frac, radio, seg, mza, lado
 ;
 
