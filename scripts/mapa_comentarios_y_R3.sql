6d477518affbc5e091371408b860b1a04d4b5780
Hernán Alperin <hernan.alperin@gmail.com>
2019-11-13 11:49:16 -0300

    Create mapa_comentarios_y_R3.sql

---
+++ scripts/mapa_comentarios_y_R3.sql
@@ -0,0 +1,27 @@
+with 
+  lados_por_mza as (
+  select prov, depto, frac, radio, mza, count(*) as cant_lados
+  from e0359.listado_segmentos
+  group by prov, depto, frac, radio, mza
+  ),
+  mzas_en_segmentos as (
+  select prov, depto, frac, radio, mza, seg, count(*) as cant_lados_en_seg
+  from e0359.listado_segmentos
+  group by prov, depto, frac, radio, mza, seg
+  ),
+  mzas_completas as (
+  select * 
+  from mzas_en_segmentos
+  natural join 
+  lados_por_mza
+  where cant_lados = cant_lados_en_seg
+  )
+select prov, depto, frac, radio, seg, mza, Null as lado
+from mzas_completas
+union
+select prov, depto, frac, radio, seg, mza, lado
+from e0359.listado_segmentos
+where (prov, depto, frac, radio, seg, mza) not in (select * from mzas_completas)
+order by prov, depto, frac, radio, seg, mza, lado
+;
+
